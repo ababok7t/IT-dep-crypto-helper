938dedeee36d74bbca42e86e545ff3e51a4cb21b
Александр Толдин <aleksandrtoldin@MacBook-Air-Aleksandr.local>
2024-11-21 08:52:42 +0300

    Initial commit

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,23 +1,5 @@
 package app
 
-import (
-	"crypto-helper/internal/infra/cache"
-	"crypto-helper/internal/services"
-	"time"
-)
-
 func RunApp() {
-	cache := cache.New(time.Second*30, time.Minute)
-
-	coins, gettingError := services.GetCoinsInfo()
-
-	if gettingError != nil {
-		panic(gettingError)
-	}
-
-	for _, coin := range coins {
-		cache.Set(coin.Symbol, coin, time.Second*30)
-	}
-
-	return
+	
 }
