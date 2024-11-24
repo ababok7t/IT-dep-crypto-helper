67a30257ec6e095f2a885ed99fd7413e7e8ab900
Александр Толдин <aleksandrtoldin@MacBook-Air-Aleksandr.local>
2024-11-25 00:16:11 +0300

    Initial commit

---
+++ internal/domain/domain.go
@@ -0,0 +1,15 @@
+package domain
+
+type Coin struct {
+	Id               string `json:"id"`
+	Symbol           string `json:"symbol"`
+	Name             string `json:"name"`
+	PriceUsd         string `json:"price_usd"`
+	PercentChange24H string `json:"percent_change_24h"`
+	PercentChange1H  string `json:"percent_change_1h"`
+	PercentChange7D  string `json:"percent_change_7d"`
+}
+
+type Coins struct {
+	Coins []Coin `json:"data"`
+}
