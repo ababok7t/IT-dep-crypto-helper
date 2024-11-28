30aba82149c7b09150e83f249ad0230f97453af4
Александр Толдин <aleksandrtoldin@MacBook-Air-Aleksandr.local>
2024-11-28 09:28:24 +0300

    Service and Cache

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,5 +1,10 @@
 package main
 
 func main() {
-
+	//envLoadingError := godotenv.Load()
+	//if envLoadingError != nil {
+	//	log.Fatal("Error loading .env file")
+	//}
+	//
+	//token := os.Getenv("TELEGRAM_BOT_TOKEN")
 }
