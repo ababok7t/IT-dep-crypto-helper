7bb01d226ac55e4f47a3593f2934cbb5fee8047f
Александр Толдин <aleksandrtoldin@MacBook-Air-Aleksandr.local>
2024-12-01 05:11:55 +0300

    Commit

--- internal/domain/coins.go
+++ internal/domain/coins.go
@@ -18,5 +18,6 @@ type Coins struct {
 
 func MakeCoinReply(coinSymbol string, coinInfo Coin, coinForecast string, pr string) string {
 	reply := fmt.Sprintf("информация о криптовалюте %s:\nназвание: %s\nцена: %s$\nизменение цены за 1 час: %s %s\nизменение цены за 24 часа: %s %s\nизменение цены за 7 дней: %s %s\nпрогноз: %s $", coinSymbol, coinInfo.Name, coinInfo.PriceUsd, coinInfo.PercentChange1H, pr, coinInfo.PercentChange24H, pr, coinInfo.PercentChange7D, pr, coinForecast)
+
 	return reply
 }
